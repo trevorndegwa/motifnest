bf9cfb7bb2bc13fad475be4b43a255d187064e2c
trevorndegwa <tntrende@gmail.com>
2024-09-05 17:37:36 +0300

    Register and Login

--- motifnest.py
+++ motifnest.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '9c7f2083cb9eb392f95efe2e53c8379a'
 
 posts = [
     {
@@ -16,14 +19,32 @@ posts = [
     }
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+	return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+	return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ test_flask_wtf.py
@@ -0,0 +1,3 @@
+# test_flask_wtf.py
+from flask_wtf import FlaskForm
+print("Flask-WTF is installed correctly.")
